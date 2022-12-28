8f9bb8e3bdff7737240778431fce261afa9d4e1d
asocream <yim0321@naver.com>
2022-12-28 14:02:33 +0900

    기본프로젝트 12-28(01)

---
+++ src/main/java/asocream/deafaultproject/DeafaultProjectApplication.java
@@ -0,0 +1,13 @@
+package asocream.deafaultproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeafaultProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeafaultProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/asocream/deafaultproject/DeafaultProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package asocream.deafaultproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeafaultProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
